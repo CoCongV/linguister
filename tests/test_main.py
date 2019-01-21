57f321a8905a03aa6cfd80714f1619d5fbe806b9
CoCong <cong.lv.yx@gmail.com>
2019-01-21 22:26:55 +0800

    remove play audio test

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,3 @@ class TestMain:
         for words in self.words:
             future = asyncio.ensure_future(run(words, False))
             loop.run_until_complete(future)
-
-        for words in self.words:
-            future = asyncio.ensure_future(run(words, True))
-            loop.run_until_complete(future)

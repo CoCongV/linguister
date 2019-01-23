53b7af5b1175620b667661c0552ed3b7b124ddd8
CoCong <cong.lv.yx@gmail.com>
2019-01-23 22:53:01 +0800

    fix test example

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,5 +7,6 @@ class TestMain:
     words = ['love', 'I love you' ,'asasdfsda']
     def test_run(self):
         for words in self.words:
-            future = asyncio.ensure_future(run(words, False))
+            future = asyncio.ensure_future(
+                run(words, False, origin='en', dest='cn', proxy=None))
             loop.run_until_complete(future)
